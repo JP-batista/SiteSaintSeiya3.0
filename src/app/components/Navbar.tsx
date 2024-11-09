13aad481bac0b0888d1580263b460b44e132a28f
JP <jpedro204jp@rede.ulbra.br>
2024-11-09 16:38:16 -0300

    Teste1-Quiz/CorreçãoTemaNavbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -15,10 +15,18 @@ export default function Navbar({ onThemeChange }: { onThemeChange: (theme: strin
   useEffect(() => {
     const savedUsername = localStorage.getItem('username');
     const savedSelectedCavaleiro = localStorage.getItem('selectedCavaleiro');
+    const savedTheme = localStorage.getItem('currentTheme');
 
     setUsername(savedUsername || 'Seiya de Pégaso');
-    setSelectedCavaleiro(savedSelectedCavaleiro ? cavaleiros.find(c => c.name === savedSelectedCavaleiro) || cavaleiros[0] : cavaleiros[0]);
+    setSelectedCavaleiro(
+      savedSelectedCavaleiro ? cavaleiros.find(c => c.name === savedSelectedCavaleiro) || cavaleiros[0] : cavaleiros[0]
+    );
+    setCurrentTheme(savedTheme || 'santuary');
   }, []);
+
+  useEffect(() => {
+    localStorage.setItem('currentTheme', currentTheme);
+  }, [currentTheme]);
 
   // Função para selecionar o tema e fechar o dropdown ao clicar
   const selectTheme = (theme: string) => {
