2b32c0161e16b201a68f6bb0dd053e4609a64ef0
JP <jpedro204jp@rede.ulbra.br>
2024-11-09 16:53:27 -0300

    TesteDeuErrado

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -15,15 +15,22 @@ export default function Navbar({ onThemeChange }: { onThemeChange: (theme: strin
   useEffect(() => {
     const savedUsername = localStorage.getItem('username');
     const savedSelectedCavaleiro = localStorage.getItem('selectedCavaleiro');
+    const savedTheme = localStorage.getItem('currentTheme');
 
     setUsername(savedUsername || 'Seiya de Pégaso');
     setSelectedCavaleiro(savedSelectedCavaleiro ? cavaleiros.find(c => c.name === savedSelectedCavaleiro) || cavaleiros[0] : cavaleiros[0]);
+    setCurrentTheme(savedTheme || 'santuary');
+    onThemeChange(savedTheme || 'santuary'); // Aplica o tema ao carregar a página
   }, []);
+
+  useEffect(() => {
+    localStorage.setItem('currentTheme', currentTheme); // Salva o tema atual no localStorage
+  }, [currentTheme]);
 
   // Função para selecionar o tema e fechar o dropdown ao clicar
   const selectTheme = (theme: string) => {
     setCurrentTheme(theme);
-    onThemeChange(theme);
+    onThemeChange(theme); // Chama a função para aplicar o tema
     setThemeDropdownOpen(false); // Fecha o dropdown ao selecionar
   };
 
@@ -60,7 +67,14 @@ export default function Navbar({ onThemeChange }: { onThemeChange: (theme: strin
   };
 
   return (
-    <header className="bg-gray-900 bg-opacity-75 shadow-lg backdrop-blur-md fixed top-0 left-0 w-full z-50">
+    <header
+      className="bg-gray-900 bg-opacity-75 shadow-lg backdrop-blur-md fixed top-0 left-0 w-full z-50"
+      style={{
+        backgroundImage: `url(/images/${currentTheme}.jpg)`, // Define o fundo com o tema atual
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
       <div className="container mx-auto flex items-center justify-between py-4 px-6">
         <div className="flex items-center">
           <Link href="/" className="flex items-center" aria-label="Logo Os Cavaleiros do Zodíaco">
